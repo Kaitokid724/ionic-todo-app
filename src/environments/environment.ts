7335abf33dded6e13ced56ccdf0a21ded258a421
Kaitokid724 <d.valencia710@gmail.com>
2026-03-29 18:19:52 -0500

    ocultar clave de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
-  production: false,
+production: false,
   firebase: {
-    apiKey: "AIzaSyDNc2waOYMBNgWuynQ4DDF50bjqDryAVCo",
-  authDomain: "todo-app-87199.firebaseapp.com",
-  projectId: "todo-app-87199",
-  storageBucket: "todo-app-87199.firebasestorage.app",
-  messagingSenderId: "1039061403237",
-  appId: "1:1039061403237:web:5ff5a54445e98e80c6288c",
-  measurementId: "G-9TZ594N0E2"
-  },
+    apiKey: 'TU_API_KEY',
+    authDomain: 'TU_PROYECTO.firebaseapp.com',
+    projectId: 'TU_PROYECTO_ID',
+    storageBucket: 'TU_PROYECTO.appspot.com',
+    messagingSenderId: 'TU_SENDER_ID',
+    appId: 'TU_APP_ID',
 };
